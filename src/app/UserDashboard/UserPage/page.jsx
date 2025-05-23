52231809cdd4985e789832972c085abdde07fcb4
israrhusain5892 <israrhusain5892@gmail.com>
2025-05-23 18:01:40 +0530

    add loader in user page

--- src/app/UserDashboard/UserPage/page.jsx
+++ src/app/UserDashboard/UserPage/page.jsx
@@ -6,24 +6,13 @@ import { useState,useEffect } from 'react';
 import { getAllUsers } from '@/Api';
 import { MdModeEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import Loader from '@/components/Loader';
 const UsersPage = () => {
 
   
-// const users = [
-//     { id: 1, name: 'John Doe', email: 'john@example.com', tasks: ['Task 1', 'Task 2'] },
-//     { id: 2, name: 'Jane Smith', email: 'jane@example.com', tasks: ['Task 3'] },
-//     { id: 3, name: 'Samuel Green', email: 'samuel@example.com', tasks: ['Task 4', 'Task 5', 'Task 6'] },
-//     { id: 4, name: 'Emma White', email: 'emma@example.com', tasks: ['Task 7'] },
-//     { id: 5, name: 'Michael Brown', email: 'michael@example.com', tasks: ['Task 8', 'Task 9'] },
-//     { id: 6, name: 'Emily Black', email: 'emily@example.com', tasks: ['Task 10'] },
-//     { id: 7, name: 'David Blue', email: 'david@example.com', tasks: ['Task 11', 'Task 12'] },
-//     { id: 8, name: 'Olivia Gray', email: 'olivia@example.com', tasks: ['Task 13'] },
-//     { id: 9, name: 'James Wilson', email: 'james@example.com', tasks: ['Task 14', 'Task 15'] },
-//     { id: 10, name: 'Lily Clark', email: 'lily@example.com', tasks: ['Task 16'] },
-//   ];
 
   const [searchQuery, setSearchQuery] = useState('');
- 
+  const [isLoading,setLoading]=useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
   const [expandedRows, setExpandedRows] = useState({});
@@ -31,9 +20,14 @@ const UsersPage = () => {
      const [filteredUsers, setFilteredUsers] = useState(users);
   useEffect(()=>{
        const getUsers=async ()=>{
+           setLoading(true)
           const response=await getAllUsers();
-          setUsers(response.data);
-          setFilteredUsers(response.data);
+          if(response.status===201 || response.status===200){
+                setLoading(false)
+                setUsers(response.data);
+                setFilteredUsers(response.data);
+          }
+          
        }
        getUsers();
   },[])
@@ -80,7 +74,7 @@ const UsersPage = () => {
   return (
     <div className={`h-[100vh] ${styles.usersPage}`}>
       <h1 className={styles.list}>Team Members</h1>
-
+      
       <div className={styles.searchWrapper}>
         <input
           type="text"
@@ -92,6 +86,7 @@ const UsersPage = () => {
       </div>
 
       <div className={styles.tableWrapper}>
+       {isLoading && <Loader/>} 
         <table className={styles.responsiveTable}>
           <thead>
             <tr>
